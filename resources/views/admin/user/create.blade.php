f3e0c792bc2808e76190a0ef41504804e446cdb6
NeyamatUllahBulbul <bbnemu98@gmail.com>
2020-02-14 22:20:10 +0600

    Templating done

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.admin.master')
+@section('content')
+    <div class="row">
+        <!-- left column -->
+        <div class="offset-3 col-md-6">
+            <!-- general form elements -->
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Add new user</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form role="form" action="{{route('user.store')}}" method="post">
+                    @csrf
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="Enter user name">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" name="email" value="{{old('email')}}" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone </label>
+                            <input type="text" name="phone" value="{{old('phone')}}" class="form-control" id="phone" placeholder="Enter your phone number">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="c_password">Confirm Password</label>
+                            <input type="password" name="password_confirmation" class="form-control" id="c_password" placeholder="Confirm Password">
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.card -->
+        </div>
+        <!--/.col (left) -->
+    </div>
+@endsection
