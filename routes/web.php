1b8e15402cb7b4f1f8ac8b88cfb13575f52f67fa
NeyamatUllahBulbul <bbnemu98@gmail.com>
2020-02-28 02:49:19 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('dashboard',function(){
 Route::resource('user','UserController');
 Route::resource('category','CategoryController')->except(['show']);
 Route::resource('author','AuthorController');
+Route::resource('post','PostController');
