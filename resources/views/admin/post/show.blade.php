59bbdcd1aa8b11c524e3530634dcf20d506316a0
NeyamatUllahBulbul <bbnemu98@gmail.com>
2020-03-16 00:55:39 +0600

    front templating done

--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -15,7 +15,7 @@
 
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Post deatails</h3>
+                    <h3 class="card-title">Post details</h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body p-0">
---
+++ resources/views/layouts/front/_head.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <title>Meranda &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=B612+Mono|Cabin:400,700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/front/fonts/icomoon/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/jquery.fancybox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/fonts/flaticon/font/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/front/css/aos.css')}}">
+    <link href="{{asset('assets/front/css/jquery.mb.YTPlayer.min.css')}}" media="all" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets/front/css/style.css')}}">
+</head>
